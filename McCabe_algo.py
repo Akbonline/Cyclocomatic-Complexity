3bbfca51b2ff1f5fc827edd1f8d808b043f87ec0
domle <domlesaca@gmail.com>
2020-04-03 17:23:43 -0700

    Update McCabe_algo.py
    Added Comments to McCabe

--- McCabe_algo.py
+++ McCabe_algo.py
@@ -1,5 +1,11 @@
 import networkx as nx
 
+# Function calculates Cyclomatic Complexity of a graph representing code
+# Input:  A NetworkX graph
+# Output:  AN integer
+# The formula:
+#       # of edges + # of nodes + 2 * # of connected components in the graph
+# Source:  https://0-ieeexplore.ieee.org.pacificatclassic.pacific.edu/document/1702388
 def Mccabe_Complexity(graph):
     return graph.number_of_edges() - graph.number_of_nodes() + 2 * nx.number_connected_components(graph)
 
