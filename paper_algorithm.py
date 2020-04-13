0eb2ba31dca1b12f0522c229cfd8baf2e8d22f56
David Samuel <david.samuel1227@gmail.com>
2020-04-12 18:37:41 -0700

    Included paper algo with networkx

--- paper_algorithm.py
+++ paper_algorithm.py
@@ -6,19 +6,34 @@ import matplotlib.pyplot as plt
 # Function calculates Cyclomatic complexity of a graph representing code
 # Input: An adjacency matrix
 # Output: Integer representing cyclomatic complexity
-# Formula = Sum of all out degree - (number of nodes with out degree greater or equal to one) + 1
+# Formula = Sum of all out degree - (number of nodes with out degree greater or equal to one)
 # Source: https://0-ieeexplore.ieee.org.pacificatclassic.pacific.edu/document/8229931
 def paper_algorithm(adjMat):
-    combMatr = adjMat * adjMat.transpose()
-    print("Adj Matrix * Transpose")
-    print(combMatr)
+    combMatr = np.dot(adjMat, adjMat.transpose())
     outDegreeGtOne = []
     sum = 0;
     for i in range(len(combMatr)):
         sum += combMatr.item(i,i)
         if (combMatr.item((i,i)) >= 1):
             outDegreeGtOne.append(combMatr.item((i,i)))
-    return (sum - len(outDegreeGtOne) + 1)
+
+    return (sum - len(outDegreeGtOne))
+
+# Function calculates Cyclomatic complexity of a graph representing code
+# Input: A network X graph
+# Output: Integer representing cyclomatic complexity
+# Formula = Sum of all out degree - (number of nodes with out degree greater or equal to one)
+# Source: https://0-ieeexplore.ieee.org.pacificatclassic.pacific.edu/document/8229931
+def p_2(G):
+    sum = 0;
+    outDegreeGtOne = []
+    for n in G:
+        od = G.out_degree(n)
+        sum += od
+        if od >= 1:
+            outDegreeGtOne.append(n)
+
+    return sum - len(outDegreeGtOne)
 
 def main():
     adjMat = np.matrix('0 1 1 0 0; 0 0 1 0 0; 0 0 0 1 1; 0 0 1 0 0; 0 0 0 0 0')
